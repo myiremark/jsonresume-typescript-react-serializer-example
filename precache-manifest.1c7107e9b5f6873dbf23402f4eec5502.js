4ba2378df5b6516b51ab9e9a3c3f37a397800cdf
Mark Graves <mark@myire.com>
2020-05-02 20:51:25 -0700

    initial commit of working version without double path rendering bug

---
+++ precache-manifest.1c7107e9b5f6873dbf23402f4eec5502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de48737a5a2e68933cdff6f1506171e4",
+    "url": "/jsonresume-typescript-react-serializer-example/index.html"
+  },
+  {
+    "revision": "3cda1871ff08bc19f15e",
+    "url": "/jsonresume-typescript-react-serializer-example/static/js/2.e16b165c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jsonresume-typescript-react-serializer-example/static/js/2.e16b165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bbe826dd582149a3a1",
+    "url": "/jsonresume-typescript-react-serializer-example/static/js/main.fc9be0a5.chunk.js"
+  },
+  {
+    "revision": "331bb112f495d64e92c2",
+    "url": "/jsonresume-typescript-react-serializer-example/static/js/runtime-main.146788fb.js"
+  }
+]);
